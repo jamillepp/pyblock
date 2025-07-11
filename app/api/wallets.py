1679c0613baa7ba17cbf7d8d07e05841611a6b1b
Jamille Peres <jamillep94@gmail.com>
2025-07-11 18:13:27 -0300

    Add create and list wallets endpoints

---
+++ app/api/wallets.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from app.db import models, schemas
+from app.core import eth, utils
+from app.db.session import get_db
+from app.core.logger import logger
+
+router = APIRouter()
+
+@router.post("/", response_model=schemas.WalletCreateResponse)
+def create_wallets(qtd: int, db: Session = Depends(get_db)):
+    logger.info(f"Request to create {qtd} wallets received")
+
+    if qtd <= 0 or qtd > 50:
+        logger.error("Invalid quantity {qtd} for wallet creation. Must be between 1 and 50.")
+        raise HTTPException(status_code=400, detail="Quantidade inválida")
+
+    wallets = []
+    for _ in range(qtd):
+        address, private_key = eth.create_wallet()
+        encrypted_key = utils.encrypt_private_key(private_key)
+
+        wallet = models.Wallet(
+            address=address,
+            private_key=encrypted_key,
+        )
+        db.add(wallet)
+        wallets.append(address)
+
+    logger.info(f"{qtd} wallets created successfully. Saving to database.")
+
+    db.commit()
+
+    logger.info("Wallets saved to database successfully.")
+
+    return {"message": f"{qtd} carteiras criadas com sucesso", "addresses": wallets}
+
+@router.get("/", response_model=list[schemas.WalletOut])
+def list_wallets(db: Session = Depends(get_db)):
+    logger.info("Request to list all wallets received")
+
+    wallets = db.query(models.Wallet).all()
+
+    logger.info(f"Retrieved {len(wallets)} wallets from the database")
+    return wallets
--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.api import wallets
+from app.db.session import engine
+from app.db import models
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
 @app.get("/")
-async def root():
-    return {"message": "Welcome to the Wallets and Transactions API"}
+def read_root():
+    return {"message": "Welcome to the PyBlock API!"}
+
+app.include_router(wallets.router, prefix="/wallets")
