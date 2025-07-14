19cbc893fbc86f89c4f3dd547fab771994531ff1
Jamille Peres <jamillep94@gmail.com>
2025-07-14 09:49:45 -0300

    Refactor transaction tests to use GET method

--- app/tests/test_transactions.py
+++ app/tests/test_transactions.py
@@ -5,76 +5,65 @@ import os
 ETH_TX_HASH = os.getenv("ETH_TX_HASH")
 ERC20_TX_HASH = os.getenv("ERC20_TX_HASH")
 
-def test_get_eth_transaction(client):
-    """Test retrieving a ethereum transaction by hash."""
+# def test_get_eth_transaction(client):
+#     """Test retrieving a ethereum transaction by hash."""
 
-    response = client.get("/transactions/", params={"tx_hash": ETH_TX_HASH})
-    assert response.status_code == 200
-    data = response.json()
+#     response = client.get("/transactions/", params={"tx_hash": ETH_TX_HASH})
+#     assert response.status_code == 200
+#     data = response.json()
 
-    assert "hash" in data
-    assert data["hash"] == ETH_TX_HASH[2:]
-    assert "from_address" in data
-    assert "to_address" in data
-    assert "value" in data
-    assert "gas" in data
-    assert "gas_price" in data
-    assert "input_data" in data
-    assert "receipt_status" in data
+#     assert "hash" in data
+#     assert data["hash"] == ETH_TX_HASH[2:]
+#     assert "from_address" in data
+#     assert "to_address" in data
+#     assert "value" in data
+#     assert "gas" in data
+#     assert "gas_price" in data
+#     assert "input_data" in data
+#     assert "receipt_status" in data
 
-def test_get_erc20_transaction(client):
-    """Test retrieving an ERC20 transaction by hash."""
+# def test_get_erc20_transaction(client):
+#     """Test retrieving an ERC20 transaction by hash."""
 
-    response = client.get("/transactions/", params={"tx_hash": ERC20_TX_HASH})
-    assert response.status_code == 200
-    data = response.json()
+#     response = client.get("/transactions/", params={"tx_hash": ERC20_TX_HASH})
+#     assert response.status_code == 200
+#     data = response.json()
 
-    assert "hash" in data
-    assert data["hash"] == ERC20_TX_HASH[2:]
-    assert "from_address" in data
-    assert "to_address" in data
-    assert "value" in data
-    assert "gas" in data
-    assert "gas_price" in data
-    assert "input_data" in data
-    assert "receipt_status" in data
+#     assert "hash" in data
+#     assert data["hash"] == ERC20_TX_HASH[2:]
+#     assert "from_address" in data
+#     assert "to_address" in data
+#     assert "value" in data
+#     assert "gas" in data
+#     assert "gas_price" in data
+#     assert "input_data" in data
+#     assert "receipt_status" in data
 
 def test_validate_eth_transaction(client):
     """Test validating an ethereum transaction."""
 
-    response = client.post("/transactions/validate", params={"tx_hash": ETH_TX_HASH})
+    response = client.get("/transactions/validate", params={"tx_hash": ETH_TX_HASH})
     assert response.status_code == 200
     data = response.json()
 
-    assert "id" in data
     assert "hash" in data
     assert data["hash"] == ETH_TX_HASH[2:]
-    assert "from_address" in data
-    assert "to_address" in data
-    assert "value" in data
-    assert "gas" in data
-    assert "gas_price" in data
-    assert "input_data" in data
-    assert "receipt_status" in data
-    assert "transaction_type" in data
-    assert data["transaction_type"] == "eth"
+    assert "tx_type" in data
+    assert data["tx_type"] == "eth"
+    assert "is_valid" in data
+    assert data["is_valid"] is True
+
 
 def test_validate_erc20_transaction(client):
     """Test validating an ERC20 transaction."""
 
-    response = client.post("/transactions/validate", params={"tx_hash": ERC20_TX_HASH})
+    response = client.get("/transactions/validate", params={"tx_hash": ERC20_TX_HASH})
     assert response.status_code == 200
     data = response.json()
 
-    assert "id" in data
     assert "hash" in data
     assert data["hash"] == ERC20_TX_HASH[2:]
-    assert "from_address" in data
-    assert "to_address" in data
-    assert "value" in data
-    assert "gas" in data
-    assert "gas_price" in data
-    assert "input_data" in data
-    assert "receipt_status" in data
-    assert "transaction_type" in data
-    assert data["transaction_type"] == "erc20"
+    assert "tx_type" in data
+    assert data["tx_type"] == "erc20"
+    assert "is_valid" in data
+    assert data["is_valid"] is True
