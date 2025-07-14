d1d56dad2281cf77248f84f220b4d70f001ab778
Jamille Peres <jamillep94@gmail.com>
2025-07-13 23:04:04 -0300

    Refactor config.py to improve environment variable handling and add Web3 provider function

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,27 @@
-from dotenv import load_dotenv
+"""Configuration settings for the application."""
+
 import os
+from dotenv import load_dotenv
+from web3 import Web3
 
 environment = os.getenv("ENVIRONMENT", "local")
-env_file = f".env.{environment}"
+ENV_FILE = f".env.{environment}"
 
-if os.path.exists(env_file):
-    load_dotenv(env_file)
+if os.path.exists(ENV_FILE):
+    load_dotenv(ENV_FILE)
 else:
     load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in the environment variables.")
 AES_KEY = os.getenv("AES_KEY")
+if not AES_KEY:
+    raise ValueError("AES_KEY is not set in the environment variables.")
+METAMASK_API_KEY = os.getenv("METAMASK_API_KEY")
+
+def get_web3_sepolia_provider():
+    """Get a Web3 provider for the Sepolia testnet."""
+    if not METAMASK_API_KEY:
+        raise ValueError("METAMASK_API_KEY is not set in the environment variables.")
+    return Web3(Web3.HTTPProvider(f'https://sepolia.infura.io/v3/{METAMASK_API_KEY}'))
