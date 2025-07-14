361c16c55ffbcf6ece50af70d95358bcccb310fb
Jamille Peres <jamillep94@gmail.com>
2025-07-13 23:07:27 -0300

    Add docstring for clarity and to session pkg

--- app/db/session.py
+++ app/db/session.py
@@ -1,3 +1,5 @@
+"""Session management for the application using SQLAlchemy."""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core import config
@@ -8,6 +10,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
+    """Dependency to get a database session."""
     db = SessionLocal()
     try:
         yield db
